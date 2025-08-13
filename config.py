5f0c5d1a72d23ff08bdadc93df5fe54ec2ac97df
Michael Reynaga <michael@blacktoaster.com>
2025-08-13 00:11:19 +0000

    move vars to config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+CLAUDE_API_KEY = os.getenv('ANTHROPIC_API_KEY', "some-api-key")
+CLAUDE_MODEL = os.getenv('CLAUDE_MODEL', "claude-3-haiku-20240307")
+
+APP_SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-in-production')
